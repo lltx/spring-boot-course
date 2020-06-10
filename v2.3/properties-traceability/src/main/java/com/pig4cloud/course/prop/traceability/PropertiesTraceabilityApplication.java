85b11fe6a51951d627eb76fa83436cfdd9ba73a0
lengleng <wangiegie@gmail.com>
2020-06-10 12:27:34 +0800

    spring boot v2.3 配置文件属性可跟踪

---
+++ v2.3/properties-traceability/src/main/java/com/pig4cloud/course/prop/traceability/PropertiesTraceabilityApplication.java
@@ -0,0 +1,13 @@
+package com.pig4cloud.course.prop.traceability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesTraceabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertiesTraceabilityApplication.class, args);
+    }
+
+}
---
+++ v2.3/properties-traceability/src/test/java/com/pig4cloud/course/prop/traceability/PropertiesTraceabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pig4cloud.course.prop.traceability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesTraceabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
